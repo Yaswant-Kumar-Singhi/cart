77d8f507422b5eb1a2d52092e8bc56e160c72eab
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-22 22:49:12 +0530

    Updates

---
+++ precache-manifest.c69cff61dac3383fa47bf887ca8c412e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf7c0c88a0a20be9ad5c648289d775f",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "44c414653d804bf8d089",
+    "url": "/cart/static/css/main.48692435.chunk.css"
+  },
+  {
+    "revision": "456ba277f52c504a1f86",
+    "url": "/cart/static/js/2.6a833c8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.6a833c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c414653d804bf8d089",
+    "url": "/cart/static/js/main.f27e453a.chunk.js"
+  },
+  {
+    "revision": "4927c2924af25d99dd24",
+    "url": "/cart/static/js/runtime-main.2c967d9a.js"
+  }
+]);
