29a3acd2fc5a0f9ee7a95ca1915786331ccbb926
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-06-23 11:00:36 +0530

    renderina a list

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-    constructor () {
-        super();
-        this.state = {
-            price : 999,
-            title : 'phone',
-            qty : 1,
-            img : ''
-        }
-       // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
+    
     increaseQuantity = () => {
         //this.state.qty += 1;
         //console.log('this',this.state);
@@ -39,8 +30,8 @@ class CartItem extends React.Component{
         })
     }
     render() {
-        console.log(this.setState)
-        const {price,title, qty} = this.state;
+        console.log('this.props',this.props)
+        const {price,title, qty} = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
